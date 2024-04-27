df9297fa7527cfee21d3cd4077e84acde933f189
KarionGranin <kariongranin@gmail.com>
2024-04-27 19:07:52 +0300

    add wordle service

--- src/app/pages/wordle/wordle.component.ts
+++ src/app/pages/wordle/wordle.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { WordleService } from './wordle.service';
 
 @Component({
   selector: 'app-wordle',
-
   templateUrl: './wordle.component.html',
   styleUrl: './wordle.component.scss',
+  providers: [WordleService],
 })
-export class WordleComponent {}
+export class WordleComponent {
+  constructor(private wardleService: WordleService) {}
+}
