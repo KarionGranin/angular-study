1d520624405bbfb43b21fe2c33721b7c55b17668
KarionGranin <kariongranin@gmail.com>
2024-05-12 20:01:04 +0300

    Заготовка сервиса для Судоку

--- src/app/pages/sudoku/sudoku.component.ts
+++ src/app/pages/sudoku/sudoku.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SudokuService } from './sudoku.service';
 
 @Component({
   selector: 'app-sudoku',
   templateUrl: './sudoku.component.html',
   styleUrl: './sudoku.component.scss',
+  providers: [SudokuService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SudokuComponent {}
+export class SudokuComponent {
+  constructor(private readonly sudokuService: SudokuService) {}
+}
